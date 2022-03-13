44204a580d23226cedf38d21e5509d83d6a26be5
Vaness <vaness13337@gmail.com>
2022-03-13 09:42:47 +0700

    Sign in responsive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,134 @@
 @extends('layouts.app')
 
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
+@endsection
+
+@section('content')
+    <main class="form-container container ">
+        <div class="container h-100 p-0">
+            <div class="row h-100 p-0 bg-main-form-custom">
+                <div class="col h-100 left-container">
+                    <form class="row g-3 register-form">
+                        <div class="header-wrapper">
+                            <h1>Sign in</h1>
+                            <p>Welcome to Articulasi</p>
+                        </div>
+                        <div class="row-md-6">
+                            <label for="inputEmail4" class="form-label">Full Name</label>
+                            <input type="text" class="form-control bg-input-form-custom custom-input" id="inputEmail4">
+                        </div>
+                        <div class="row-md-6">
+                            <label for="inputEmail4" class="form-label">Email</label>
+                            <input type="email" class="form-control bg-input-form-custom custom-input" id="inputEmail4">
+                        </div>
+                        <div class="remember-forgot-container">
+                            <div class="form-check remember-me">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <a href="">Forget password?</a>
+                        </div>
+                        <div class="col-auto w-100 btn-container">
+                            <button type="submit" class="btn bg-main-custom d-flex login-btn" >
+                                <p>Login</p>
+                                <img src="{{asset('images/icon/Arrow.png')}}" alt="">
+                            </button>
+                        </div>
+                        <p class="login-link-container text-center ">Don’t have any account? <b>Sign up</b></p>
+                    </form>
+                </div>
+                <div class="col p-0 right-container">
+                    <div class="image-wrapper"></div>
+                </div>
+            </div>
+            </div>
+    </main>
+@endsection
+
+
+
+{{-- @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
+
+
+
+
+{{-- @extends('layouts.app')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -70,4 +199,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
